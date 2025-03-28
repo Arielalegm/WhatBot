a378c5143e6069fe2af3f2d05e84529cf91b7f06
Ariel Alejandro Gálvez Mirabal <144627936+Arielalegm@users.noreply.github.com>
2025-03-28 19:45:44 -0400

    Update reminderService.ts

--- src/services/reminderService.ts
+++ src/services/reminderService.ts
@@ -27,7 +27,7 @@ export class ReminderService {
 
             // Crear fecha directamente en UTC-4
             const reminderDate = new Date(this.currentYear, month - 1, day, hours, minutes);
-            const utcMinus4Date = new Date(reminderDate.getTime() - (4 * 60 * 60 * 1000));
+            const utcMinus4Date = new Date(reminderDate.getTime() - (1 * 60 * 60 * 1000));
             return utcMinus4Date;
         } catch (error) {
             return null;
